c4486fa10bd5a9184d12b0ae8c7dad2bcfdbd37d
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-05-28 02:04:15 +0200

    person class derived

--- Implementacija/Data/ApplicationDbContext.cs
+++ Implementacija/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ooadproject.Models;
 
 namespace ooadproject.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<Person>
+    public class ApplicationDbContext : IdentityDbContext<Person, IdentityRole<int>, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
