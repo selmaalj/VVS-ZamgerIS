6ba5baaf29adbf81ec1f6800d691a4f3f377cea9
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-06-11 18:24:59 +0200

    exam logic

---
+++ Implementacija/Models/ExamManager.cs
@@ -0,0 +1,38 @@
+﻿using ooadproject.Data;
+using ooadproject.Sheets;
+using System.Net.WebSockets;
+
+namespace ooadproject.Models
+{
+    public class ExamManager
+    {
+        private readonly ApplicationDbContext _context;
+        public ExamManager(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task SaveExamResults(Exam exam, string link)
+        {
+            var results = SheetsFacade.GetExamResults(link);
+
+            foreach (KeyValuePair<int, double> result in results)
+            {
+                var student =  _context.Student.FirstOrDefault(s => s.Index == result.Key);
+                if (student != null)
+                {
+                    var newExam = new StudentExam();
+                    newExam.Exam = exam;
+                    newExam.ExamID = exam.ID;
+                    var course = _context.StudentCourse.FirstOrDefault(sc => sc.StudentID == student.Id && sc.CourseID == exam.CourseID);
+                    newExam.Course = course;
+                    newExam.CourseID = course.ID;
+
+                    newExam.IsPassed = result.Value >= exam.MinimumPoints;
+                    newExam.PointsScored = result.Value;
+
+                    await _context.AddAsync(newExam);
+                }
+            }
+        }
+    }
+}
