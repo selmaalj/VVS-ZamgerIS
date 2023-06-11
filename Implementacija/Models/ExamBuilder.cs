7974fb2d3e2f8187f34a9fc1add0d180307e4705
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-06-11 08:28:23 +0200

    dodana klasa ExamBuilder

---
+++ Implementacija/Models/ExamBuilder.cs
@@ -0,0 +1,38 @@
+﻿namespace ooadproject.Models
+{
+    public class ExamBuilder : IExamBuilder
+    {
+        private Exam exam = new Exam();
+
+        public ExamBuilder() { }
+        public Exam build()
+        {
+            return exam;
+        }
+
+        public void setCourse(Course course)
+        {
+            exam.Course = course;
+        }
+
+        public void setDate(DateTime time)
+        {
+            exam.Time = time;
+        }
+
+        public void setExamType(ExamType type)
+        {
+            exam.Type = type;
+        }
+
+        public void setMinimumPoints(double points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setTotalPoints(double points)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Implementacija/Models/IExamBuilder.cs
+++ Implementacija/Models/IExamBuilder.cs
@@ -2,11 +2,11 @@
 {
     public interface IExamBuilder
     {
-        public IExamBuilder setExamType(ExamType examType);
-        public IExamBuilder setTotalPoints(double points);
-        public IExamBuilder setDate(DateTime date);
-        public IExamBuilder setCourse(Course course);
-        public IExamBuilder setMinimumPoints(double points);
+        public void setExamType(ExamType examType);
+        public void setTotalPoints(double points);
+        public void setDate(DateTime date);
+        public void setCourse(Course course);
+        public void setMinimumPoints(double points);
         Exam build();
     }
 }
