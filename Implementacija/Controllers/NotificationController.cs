04cc8d44db53533c9837baf7f3fccc859b6e0dcd
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-06-11 12:41:46 +0200

    notif control

---
+++ Implementacija/Controllers/NotificationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ooadproject.Data;
+using ooadproject.Models;
+
+namespace ooadproject.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Person> _userManager;
+
+        public NotificationController(ApplicationDbContext context, UserManager<Person> userManager)
+        {
+            _context = context;
+            _userManager = userManager; 
+        }
+
+        // GET: Notification
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var notifications = _context.Notification.Include(n => n.Recipient).Where(n => n.RecipientID == user.Id).OrderByDescending(n => n.ID);
+            return View(await notifications.ToListAsync());
+        }
+
+        
+    }
+}
