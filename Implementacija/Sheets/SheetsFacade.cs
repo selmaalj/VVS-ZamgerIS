14a7511dd9707eec9b82445f79dae175a25e8562
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-06-11 17:30:59 +0200

    facade

---
+++ Implementacija/Sheets/SheetsFacade.cs
@@ -0,0 +1,61 @@
+﻿namespace ooadproject.Sheets
+{
+    public class SheetsFacade
+    {
+        private static GoogleSheetsManager _manager;
+
+        public SheetsFacade()
+        {
+
+        }
+
+        public static string ExtractSpreadsheetId(string link)
+        {
+            // Extract the spreadsheet ID from the link
+            string spreadsheetId = string.Empty;
+            try
+            {
+                Uri uri = new Uri(link);
+                string path = uri.AbsolutePath;
+                string[] segments = path.Split('/');
+                spreadsheetId = segments[segments.Length - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error extracting spreadsheet ID: " + ex.Message);
+            }
+
+            return spreadsheetId;
+        }
+
+        public static Dictionary<int, int> GetExamResults(string link)
+        {
+            string id = ExtractSpreadsheetId(link);
+
+            _manager = new GoogleSheetsManager(id);
+            
+            var stringResults =  _manager.GetExam();
+            
+            var results = new Dictionary<int, int>();
+
+           
+                foreach (var kvp in stringResults)
+                {
+                    if (int.TryParse(kvp.Key, out int parsedKey) && int.TryParse(kvp.Value, out int parsedValue))
+                    {
+                        results.Add(parsedKey, parsedValue);
+                    }
+                    else
+                    {
+                        // Handle parsing errors if necessary
+                        Console.WriteLine($"Greška u formatu rezultata");
+                    }
+                }
+
+
+             
+            return results;
+        }
+
+    }
+}
