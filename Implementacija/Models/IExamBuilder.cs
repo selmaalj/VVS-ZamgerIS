001a6e4a0133e58730e05d74dcfc8c97ecb5fefc
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-06-11 08:00:05 +0200

    dodan interfejs IExamBuilder

---
+++ Implementacija/Models/IExamBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace ooadproject.Models
+{
+    public interface IExamBuilder
+    {
+        public IExamBuilder setExamType(ExamType examType);
+        public IExamBuilder setTotalPoints(double points);
+        public IExamBuilder setDate(DateTime date);
+        public IExamBuilder setCourse(Course course);
+        public IExamBuilder setMinimumPoints(double points);
+        Exam build();
+    }
+}
