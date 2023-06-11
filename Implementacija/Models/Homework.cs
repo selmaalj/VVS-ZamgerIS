0f3a80eb24161a053aaadc79da05cbd2e79c15d2
Edwin <egraca1@etf.unsa.ba>
2023-06-11 17:52:26 +0200

    validation Homework

--- Implementacija/Models/Homework.cs
+++ Implementacija/Models/Homework.cs
@@ -10,9 +10,12 @@ namespace ooadproject.Models
 
         [ForeignKey("Course")]
         public int CourseID { get; set; }
+        [Required]
         public Course? Course { get; set; }
-
+        [ValidateDateFuture]
+        [DataType(DataType.Date)]
         public DateTime Deadline { get; set; }
+        [Range(0,100,ErrorMessage = "Ne moze zadaca imati poene van opsega od 0 do 100!")]
         public double TotalPoints { get; set; }
         public string Description { get; set; }
 
---
+++ Implementacija/Models/ValidateDateFuture.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ooadproject.Models
+{
+    public class ValidateDateFuture : ValidationAttribute
+    {
+        protected override ValidationResult IsValid
+        (object date, ValidationContext validationContext)
+        {
+            return ((DateTime)date >= DateTime.Now)
+            ? ValidationResult.Success
+            : new ValidationResult("Mora rok zadace biti u buducnosti!");
+        }
+    }
+}
