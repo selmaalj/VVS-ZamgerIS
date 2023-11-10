acc5a9d12a63fee1ca18e37a41e72ccbb8cc8af0
Ajdin Suta <asuta2@etf.unsa.ba>
2023-11-10 21:18:07 +0100

    Dodano mocanje za unošenje korisnika u fejk bazu

--- TestProject1/TeacherControllerTests.cs
+++ TestProject1/TeacherControllerTests.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using ooadproject.Controllers;
 using ooadproject.Data;
 using ooadproject.Models;
+
 
 
 namespace ProjectTests
@@ -13,6 +17,10 @@ namespace ProjectTests
     {
         private ApplicationDbContext _context;
         private TeacherController _teacherController;
+        private Mock<ApplicationDbContext> _mockContext;
+        private Mock<Microsoft.AspNetCore.Identity.UserManager<Person>> _mockUserManager;
+        private Mock<Microsoft.AspNetCore.Identity.IPasswordHasher<Person>> _mockPasswordHasher;
+
 
         [TestInitialize]
         public void Setup()
@@ -21,6 +29,10 @@ namespace ProjectTests
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
             .Options;
+
+            _mockContext = new Mock<ApplicationDbContext>(options);
+            _mockUserManager = new Mock<Microsoft.AspNetCore.Identity.UserManager<Person>>(new Mock<Microsoft.AspNetCore.Identity.IUserStore<Person>>().Object, null, null, null, null, null, null, null, null);
+            _mockPasswordHasher = new Mock<IPasswordHasher<Person>>();
 
             _context = new ApplicationDbContext(options);
             _teacherController = new TeacherController(_context, null, null);
@@ -63,5 +75,48 @@ namespace ProjectTests
             // Assert
             Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
+
+        [TestMethod]
+        public async Task Create_ReturnsViewResult()
+        {
+            // Arrange
+
+            // Act
+            var result = _teacherController.Create();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public async Task Create_ValidModel_ReturnsRedirectToActionResult()
+        {
+            // Arrange
+            var controller = new TeacherController(_mockContext.Object, _mockUserManager.Object, _mockPasswordHasher.Object);
+            var teacher = new Teacher { Title = "Mr.", FirstName = "John", LastName = "Doe", UserName = "johndoe", Email = "johndoe@example.com" };
+
+            // Act
+            var result = await controller.Create(teacher) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+        }
+        [TestMethod]
+        public async Task Create_InvalidModel_ReturnsViewResultWithModel()
+        {
+            // Arrange
+            var controller = new TeacherController(_mockContext.Object, _mockUserManager.Object, _mockPasswordHasher.Object);
+            var teacher = new Teacher { Title = "Mr.", FirstName = "John", LastName = "Doe", UserName = "johndoe", Email = "johndoe@example.com" };
+            controller.ModelState.AddModelError("error", "some error");
+
+            // Act
+            var result = await controller.Create(teacher) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(teacher, result.Model);
+        }
+
     }
 }
