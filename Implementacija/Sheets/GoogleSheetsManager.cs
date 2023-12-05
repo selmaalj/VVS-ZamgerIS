0fa42cceaf89a4d29841809a50684995054f36f4
Ajdin Suta <asuta2@etf.unsa.ba>
2023-12-05 19:41:50 +0100

    Dodate osnove za google api

---
+++ Implementacija/Sheets/GoogleSheetsManager.cs
@@ -0,0 +1,16 @@
+﻿using Google.Apis.Sheets.v4.Data;
+
+namespace ooadproject.Sheets
+{
+    public class GoogleSheetsManager
+    {
+        public GoogleSheetsManager(string _spreadsheetId)
+        {
+
+        }
+        public Dictionary<string, string> GetExam()
+        {
+            return null;
+        }
+    }
+}
---
+++ Implementacija/Sheets/SheetsFacade.cs
@@ -0,0 +1,19 @@
+﻿namespace ooadproject.Sheets
+{
+    public class SheetsFacade
+    {
+        public SheetsFacade()
+        {
+
+        }
+        public static string ExtractSpreadsheetId(string link)
+        {
+            return null;
+        }
+        public static Dictionary<int, double> GetExamResults(string link)
+        {
+            return null;
+        }
+    }    
+
+}
